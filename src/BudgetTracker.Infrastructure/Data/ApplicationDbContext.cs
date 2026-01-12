a199277773f124b9d98f695faed76cf0f0247081
Wojciech Jackowiak <wojciechjackowiak26@gmail.com>
2026-01-12 20:48:24 +0100

    Infrastructure basic implementation

--- src/BudgetTracker.Infrastructure/Data/ApplicationDbContext.cs
+++ src/BudgetTracker.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using BudgetTracker.Application.Common.Interfaces;
+using BudgetTracker.Domain.Common;
+using BudgetTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BudgetTracker.Infrastructure.Data
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : DbContext, IBudgetTrackerDbContext
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> Users{ get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public DbSet<BudgetLimit> BudgetLimits { get; set; }
+
+        public DbSet<RecurringTransaction> RecurringTransactions { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>()
+                            .Where(e => e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                entry.Entity.MarkAsModified(); 
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }    
 }
